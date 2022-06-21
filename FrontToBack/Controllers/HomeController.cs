894c0f1f27d9408def437353d3b33085996d21de
FaridMammadov60 <faridmma@code.edu.az>
2022-06-21 05:27:13 -0700

    add task

---
+++ FrontToBack/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using FrontToBack.DAL;
+using FrontToBack.Models;
+using FrontToBack.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FrontToBack.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _context;
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Slider> sliders = _context.Sliders.ToList();
+            HomeVM homeVM = new HomeVM();
+            homeVM.Sliders = sliders;
+            homeVM.SliderContent = _context.SliderContents.FirstOrDefault();
+            homeVM.Categories = _context.Categories.ToList();
+            homeVM.Products= _context.Products.Include(p=>p.Category).ToList();
+
+            return View(homeVM);
+        }
+        public IActionResult Detail(int? id)
+        {
+            if(id == 0)
+            {
+                return NotFound();
+            }
+            Product dbProduct = _context.Products.FirstOrDefault(p=>p.Id==id);
+            if(dbProduct == null)
+            {
+                return NotFound();
+            }
+            return View(dbProduct);
+        }
+    }
+}
