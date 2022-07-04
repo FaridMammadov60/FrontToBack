f611c2fe0db3f6a63ded02cca32d3360817ebc9a
FaridMammadov60 <faridmma@code.edu.az>
2022-07-04 13:54:38 -0800

    add adminpanel crud

--- FrontToBack/Areas/AdminPanel/Controllers/CategoryController.cs
+++ FrontToBack/Areas/AdminPanel/Controllers/CategoryController.cs
@@ -26,22 +26,85 @@ namespace FrontToBack.Areas.AdminPanel.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult Create(Category category)
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Category category)
         {
             if (!ModelState.IsValid)
             {
                 return View();
             }
+            bool dbCategory = _context.Categories.Any(n => n.Name.ToLower()
+            .Trim() == category.Name.ToLower().Trim());
+            if (dbCategory)
+            {
+                ModelState.AddModelError("Name", "This name is exist");
+                return View();
+            }
+            Category newCategory = new Category
+            {
+                Name = category.Name,
+                Desc = category.Desc
+            };
+            await _context.Categories.AddAsync(newCategory);
+
+            await _context.SaveChangesAsync();
+
 
             //return View();
-            return Content($"{category.Name} {category.Desc}");
+            return RedirectToAction("index");
         }
         public async Task<IActionResult> Detail(int? id)
         {
             if (id == null) return NotFound();
             Category dbCategory = await _context.Categories.FindAsync(id);
-            if(dbCategory == null) return NotFound();
+            if (dbCategory == null) return NotFound();
             return View(dbCategory);
+        }
+
+        public async Task<IActionResult> Update(int? id)
+        {
+            if (id == null) return NotFound();
+            Category dbCategory = await _context.Categories.FindAsync(id);
+            if (dbCategory == null) return NotFound();
+            return View(dbCategory);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            Category dbCategory = _context.Categories.FirstOrDefault(x => x.Id == category.Id);
+            if (dbCategory == null)
+            {
+                return View();
+            }
+            Category dbCategoryName = _context.Categories.FirstOrDefault
+                (x => x.Name.ToLower().Trim() == category.Name.ToLower().Trim());
+
+            if (dbCategoryName != null)
+            {
+                if (dbCategoryName.Name != dbCategory.Name)
+                {
+                    ModelState.AddModelError("Name", "This name is exist");
+                    return View();
+                }
+            }
+            dbCategory.Name = category.Name;
+            dbCategory.Desc = category.Desc;
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+            Category dbCategory = await _context.Categories.FindAsync(id);
+            if (dbCategory == null) return NotFound();
+            _context.Categories.Remove(dbCategory);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
     }
 }
