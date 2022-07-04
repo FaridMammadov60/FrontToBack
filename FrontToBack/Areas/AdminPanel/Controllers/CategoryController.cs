d5d8b7553fda644a6193e83c19ec29896c0b08d4
FaridMammadov60 <faridmma@code.edu.az>
2022-07-04 00:34:21 -0800

    add adminpanel category detail and create

---
+++ FrontToBack/Areas/AdminPanel/Controllers/CategoryController.cs
@@ -0,0 +1,47 @@
+﻿using FrontToBack.DAL;
+using FrontToBack.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontToBack.Areas.AdminPanel.Controllers
+{
+    [Area("AdminPanel")]
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _context;
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categories = _context.Categories.ToList();
+            return View(categories);
+        }
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            //return View();
+            return Content($"{category.Name} {category.Desc}");
+        }
+        public async Task<IActionResult> Detail(int? id)
+        {
+            if (id == null) return NotFound();
+            Category dbCategory = await _context.Categories.FindAsync(id);
+            if(dbCategory == null) return NotFound();
+            return View(dbCategory);
+        }
+    }
+}
--- FrontToBack/Models/Category.cs
+++ FrontToBack/Models/Category.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace FrontToBack.Models
 {
     public class Category
     {
         public int Id { get; set; }
+        [Required, MaxLength(50)]
         public string Name { get; set; }
+        [Required, MinLength(1)]
+        public string Desc { get; set; }
         public List<Product> Products { get; set; }
     }
 }
